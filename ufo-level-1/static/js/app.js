377c3c39b6bc09cf382f5d388b6d6447d3546890
aaron28 <aaron.lee28@outlook.com>
2021-04-05 22:38:23 +0700

    commit

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -13,3 +13,43 @@ data.forEach((ufoSightings) => {
     });
 });
 
+
+function mySearch() {
+    var input, table, tr, td, i, txtValue;
+    input = document.getElementById("datetime").value;
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+
+
+
+// function mySearch() {
+//     var input, table, tr, td, i, txtValue;
+//     input = document.getElementById("datetime").value;
+//     table = document.getElementById("ufo-table");
+//     tr = table.getElementsByTagName("tr");
+
+//     for (i = 0; i < tr.length; i++) {
+//         td = tr[i].getElementsByTagName("td")[1];
+//         if (td) {
+//           txtValue = td.textContent || td.innerText;
+//           if (txtValue > -1) {
+//             tr[i].style.display = "";
+//           } else {
+//             tr[i].style.display = "none";
+//           }
+//         }
+//       }
+//     }
